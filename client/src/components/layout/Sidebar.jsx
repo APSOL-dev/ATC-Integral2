a119fa8cd6cfbf3bd71faa5898dd01c6c48c3a73
RenataAPSOL <renatamorano.apsol@gmail.com>
2026-09-22 10:09:29 -0300

    chore(release): actualizar version de la aplicacion a v2.5

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -93,7 +93,7 @@ export default function Sidebar() {
           </button>
 
           <div className="text-center text-[10px] text-white/30 font-bold mt-3 tracking-widest uppercase">
-            v2.4
+            v2.5
           </div>
         </div>
       </div>
